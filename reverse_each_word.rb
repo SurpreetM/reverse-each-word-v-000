07100ebd158b7f2e8fe60c104141d59dba2a4398
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-01 15:48:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word (sentence)
   new_array = sentence.split(" ")
 
-  new_array.collect do |word|
-    word.reverse
-  end
+  new_array.collect { |word|
+    word.reverse }.join (" ")
 
 end
